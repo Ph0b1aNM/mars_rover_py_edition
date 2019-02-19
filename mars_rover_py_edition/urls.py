0a5bd17e42ae7b0589c828424c9af9ecfe23972e
Nicolo Marini <nic.ph0b1a@gmail.com>
2019-02-19 22:52:30 +0200

    Adding db model

--- mars_rover_py_edition/urls.py
+++ mars_rover_py_edition/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('mr_app.urls')),
     path('mr_app/', include('mr_app.urls')),
     path('admin/', admin.site.urls),
 ]
--- mr_app/models.py
+++ mr_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# Mars Rover Input Table
+
+class MRInputs(models.Model):
+    sopx = models.IntegerField(default=0)
+    sopy = models.IntegerField(default=0)
+    r1px = models.IntegerField(default=0)
+    r1py = models.IntegerField(default=0)
+    r1pface = models.CharField(max_length=50)
+    inst1 = models.CharField(max_length=50)
+    r2px = models.IntegerField(default=0)
+    r2py = models.IntegerField(default=0)
+    r2pface = models.CharField(max_length=50)
+    inst2 = models.CharField(max_length=50)
