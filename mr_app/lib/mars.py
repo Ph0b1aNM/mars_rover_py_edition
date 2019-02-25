cc992d0f935c6d45b533d9f29e7cf05c1651cb2f
Nicolo Marini <nic.ph0b1a@gmail.com>
2019-02-25 18:12:16 +0200

    Adding lib files

---
+++ mr_app/lib/mars.py
@@ -0,0 +1,9 @@
+class Mars(object):
+    
+    def __init__(self, x, y):
+        """
+        Initializes a Mars-object.
+        """
+        self.x = x
+        self.y = y
+        self.occupied = []
